322126d13947f1617cdab9cf05926ddccd36a019
houssemabidi12 <abidi.houssem@esprit.tn>
2023-02-14 21:34:51 +0100

    ExtractMostLikedPostData, ExtractProfileWithMostFollowersData, ExtractProfileWithMostPostsData Updated

--- src/main/scala/GoldLayer/ExtractMostLikedPostData.scala
+++ src/main/scala/GoldLayer/ExtractMostLikedPostData.scala
@@ -4,7 +4,7 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 object ExtractMostLikedPostData {
   def ExtractMostLikedPost(spark: SparkSession, initialData: DataFrame): DataFrame = {
     initialData.createOrReplaceTempView("posts_view")
-    val mostLikedPost = spark.sql("SELECT PostId, likes_count  FROM posts_view WHERE likes_count = (SELECT MAX(likes_count) FROM posts_view)")
+    val mostLikedPost = spark.sql("SELECT PostId, MAX(likes_count) FROM posts_view")
     mostLikedPost
   }
 }

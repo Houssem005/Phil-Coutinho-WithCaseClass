b63c2c32f63d44790e3ccf28d690dc4f89cf5d8b
houssemabidi12 <abidi.houssem@esprit.tn>
2023-02-28 21:12:26 +0100

    appended all data

--- src/main/scala/PhilCoutinhoMain.scala
+++ src/main/scala/PhilCoutinhoMain.scala
@@ -22,37 +22,37 @@ object PhilCoutinhoMain {
     val inputData = spark.read.option("multiline", value = true).json("phil.coutinho-1.json")
     //Extract Comments Data
     val commentsData = ExtractComments(inputData)
-    commentsData.write.parquet("E:\\dataset\\phil.Coutinho\\SilverLayer\\Comments")
+    commentsData.write.mode("append").parquet("E:\\dataset\\phil.Coutinho\\SilverLayer\\Comments")
     //Extract Profile Data
     val profilesData = ExtractProfiles(inputData)
-    profilesData.write.parquet("E:\\dataset\\phil.Coutinho\\SilverLayer\\Profiles")
+    profilesData.write.mode("append").parquet("E:\\dataset\\phil.Coutinho\\SilverLayer\\Profiles")
     //Extract Posts Data
     val postsData = ExtractPosts(inputData)
-    postsData.write.parquet("E:\\dataset\\phil.Coutinho\\SilverLayer\\Posts")
+    postsData.write.mode("append").parquet("E:\\dataset\\phil.Coutinho\\SilverLayer\\Posts")
     //Extract Most Commenting User
     val mostCommentingUsers = ExtractMostCommentingUsers(spark, commentsData)
-    mostCommentingUsers.write.parquet("E:\\dataset\\phil.Coutinho\\GoldLayer\\MostCommentingUsers")
+    mostCommentingUsers.write.mode("append").parquet("E:\\dataset\\phil.Coutinho\\GoldLayer\\MostCommentingUsers")
     //Extract Most Liked Post
     val mostLikedPost = ExtractMostLikedPost(spark,postsData)
     mostLikedPost.write.mode("append").parquet("E:\\dataset\\phil.Coutinho\\GoldLayer\\MostLikedPost")
     //Extract profile With Most Followers
     val mostFollowedProfile = ExtractProfileWithMostFollowers(spark,profilesData)
-    mostFollowedProfile.write.parquet("E:\\dataset\\phil.Coutinho\\GoldLayer\\MostFollowedProfile")
+    mostFollowedProfile.write.mode("append").parquet("E:\\dataset\\phil.Coutinho\\GoldLayer\\MostFollowedProfile")
     //Extract Profile With Most Posts
     val profileWithMostPosts = ExtractProfileWithMostPosts(spark, profilesData)
-    profileWithMostPosts.write.parquet("E:\\dataset\\phil.Coutinho\\GoldLayer\\ProfileWithMostPosts")
+    profileWithMostPosts.write.mode("append").parquet("E:\\dataset\\phil.Coutinho\\GoldLayer\\ProfileWithMostPosts")
     //Order Posts data ascending by timestamp
     val OrderedPosts = orderPosts(spark, postsData)
-    OrderedPosts.write.parquet("E:\\dataset\\phil.Coutinho\\GoldLayer\\OrderedPosts")
+    OrderedPosts.write.mode("append").parquet("E:\\dataset\\phil.Coutinho\\GoldLayer\\OrderedPosts")
     //Sort The Comment by date(timestamp)
     val SortedComments = sortCommentsByDate(spark, commentsData)
-    SortedComments.write.parquet("E:\\dataset\\phil.Coutinho\\GoldLayer\\SortedComments")
+    SortedComments.write.mode("append").parquet("E:\\dataset\\phil.Coutinho\\GoldLayer\\SortedComments")
     //Search Posts by username
     val searchResults = SearchPostByUsername(spark, postsData, "phil.coutinho")
     searchResults.write.mode("append").parquet("E:\\dataset\\phil.Coutinho\\GoldLayer\\searchResults")
     //Extract likes and dates data(work I added before the merge of the pull request)
     val LikesByDates = ExtractLikesWithDate(spark, postsData)
-    LikesByDates.write.parquet("E:\\dataset\\phil.Coutinho\\GoldLayer\\LikesByDates")
+    LikesByDates.write.mode("append").parquet("E:\\dataset\\phil.Coutinho\\GoldLayer\\LikesByDates")
 
   }
 }
